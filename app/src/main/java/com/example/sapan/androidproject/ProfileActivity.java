51c8cd517788839e9e646b05b2a8b319bd924381
SAPAN10 <sapankumarkar@gmail.com>
2017-05-22 14:07:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sapan/androidproject/ProfileActivity.java
@@ -0,0 +1,26 @@
+package com.example.sapan.androidproject;
+
+import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sapan.androidproject.databinding.ActivityProfileBinding;
+
+public class ProfileActivity extends AppCompatActivity {
+    ActivityProfileBinding binding;
+    SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
+        preferences = getSharedPreferences("SrishtiLogin", MODE_PRIVATE);
+
+        binding.tvName.setText(preferences.getString("userName",null));
+        binding.tvPhone.setText(preferences.getString("phone",null));
+        binding.tvEmailId.setText(preferences.getString("email",null));
+        binding.tvDob.setText(preferences.getString("dob",null));
+
+    }
+}
